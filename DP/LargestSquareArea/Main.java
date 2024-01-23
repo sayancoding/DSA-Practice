3052e4004bd68c1fbec91554adc94b6d5acf62e0
sayancoding <sayanmaity007@gmail.com>
2024-01-23 11:50:55 +0530

    maxSquare area dp

--- DP/LargestSquareArea/Main.java
+++ DP/LargestSquareArea/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,20 +9,28 @@ public class Main {
         };
         int[] ans = new int[1];
         ans[0] = 0;
-        minArea(matrix, 0, 0, ans);
+        
+        int[][] dp = new int[matrix.length+1][matrix[0].length+1];
+        for (int[] row: dp)
+        Arrays.fill(row, -1);
+
+        minArea(matrix, 0, 0, ans, dp);
         System.out.println(ans[0]);
     }
-    public static int minArea(int[][] matrix, int i, int j,int[] ans){
+    public static int minArea(int[][] matrix, int i, int j,int[] ans,int[][] dp){
         if(i >= matrix.length || j >= matrix[0].length) return 0;
         
+        if(dp[i][j] != -1) return dp[i][j];
         
         if(matrix[i][j] == 1){
-            int right = minArea(matrix, i, j+1, ans);
-            int diagonal = minArea(matrix, i+1, j+1, ans);
-            int down = minArea(matrix, i+1, j, ans);
-            int subAns = 1 + Math.min(Math.min(diagonal, down),right);
-            ans[0] = Math.max(ans[0], subAns); 
-            return subAns;
+            int right = minArea(matrix, i, j+1, ans, dp);
+            int diagonal = minArea(matrix, i+1, j+1, ans, dp);
+            int down = minArea(matrix, i+1, j, ans, dp);
+            
+            dp[i][j] = 1 + Math.min(Math.min(diagonal, down),right);
+            
+            ans[0] = Math.max(ans[0], dp[i][j]); 
+            return dp[i][j];
         }else{
             return 0;
         }
